0320c814dd0eee864f1ec0fd68daf4cf3f0071f8
gulsharn <rvk_singh@outlook.com>
2019-03-18 02:24:55 +0530

    cognito auth

---
+++ examples/login.php
@@ -0,0 +1,28 @@
+<?php
+error_reporting(-1);
+ini_set('display_errors', 'On');
+use pmill\AwsCognito\CognitoClient;
+
+use pmill\AwsCognito\Exception\ChallengeException;
+use pmill\AwsCognito\Exception\PasswordResetRequiredException;
+
+/** @var CognitoClient $client */
+$client = require(__DIR__ . '/bootstrap.php');
+
+$username = 'ashok.programmers@gmail.com';
+$password = 'Ashok!123';
+
+try {
+
+    $authenticationResponse = $client->authenticate($username, $password);
+    die('sdsd');
+
+} catch (ChallengeException $e) {
+    if ($e->getChallengeName() === CognitoClient::CHALLENGE_NEW_PASSWORD_REQUIRED) {
+        $authenticationResponse = $client->respondToNewPasswordRequiredChallenge($username, 'password_new', $e->getSession());
+    }
+} catch (PasswordResetRequiredException $e) {
+    die("PASSWORD RESET REQUIRED");
+}
+
+var_dump($authenticationResponse);
