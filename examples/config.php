5ebcaec6408f9259174d2d87e0615ecc87080fff
gulsharn <rvk_singh@outlook.com>
2019-03-19 01:00:23 +0530

    add config file

---
+++ examples/config.php
@@ -0,0 +1,13 @@
+<?php
+// copy this file to config.php
+return [
+	'credentials' => [
+		'key' => 'AKIAJUIGD23FTO4AWCDA',
+		'secret' => 'dMU8PMi+2d4LkZH09VDKWUiBSOEIabYDatQQvmdU',
+	],
+	'region' => 'ap-south-1',
+	'version' => 'latest',
+	'app_client_id' => '75dhpalqg47c0b7tt2l6mk7bp8',
+	'app_client_secret' => 'hs50m02k8mp74i5oksh2qkhs3qta7j75ffo14c3a80hvt4qmg2u',
+	'user_pool_id' => 'ap-south-1_30G8nuOfN',
+];
